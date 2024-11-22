3bab541a5730a9cd9aad073ac06c764ec84215ad
lute47lillo <lutelillocs47@gmail.com>
2024-11-22 15:16:25 -0500

    Cleaning up util file

--- src/util.py
+++ src/util.py
@@ -1,5 +1,4 @@
 import argparse
-import networkx as nx
 import numpy as np
 import torch
 import os
@@ -28,35 +27,17 @@ def set_args():
     argparser.add_argument('--current_run', type=int, help='Current experimental run if multiple runs', default=0)
     
     # Experimental Hyperparameters
+    argparser.add_argument('--generations', type=int, help='Nº of generations to run the GA.', default=100)
     argparser.add_argument('--pop_size', type=int, help='Population Size (could be used as fixed parameter in many settings)', default=100)
     argparser.add_argument('--mutation_rate', type=float, help='Mutation Rate (could be used as fixed parameter in many settings)', default=0.01)
     argparser.add_argument('--inbred_threshold', type=int, help='Inbreeding Threshold. Below threshold is considered inbreeding. \
-                            Minimum genetic (Hamming) distance required to allow mating', default=5)
+                            Minimum genetic (tree-edit) distance required to allow mating', default=5)
     argparser.add_argument('--tournament_size', type=int, help='Nº of individuals to take part in the Tournament selection', default=3)
     argparser.add_argument('--exp_num_runs', type=int, help='Nº of experimental runs. (Fixed hyperparameters)', default=5)
-    
-    # Moving Peaks Benchmark hyperparameters
-    argparser.add_argument('--dimensions', type=int, help='GA dimensions. Used in Rugged benchmarks like MPL', default=100) 
-    argparser.add_argument('--collapse_threshold', type=float, help='TODO', default=0.2) 
-    argparser.add_argument('--collapse_fraction', type=float, help='TODO', default=0.1) 
-    argparser.add_argument('--mpl_shift_interval', type=int, help='nº of generations for shifting global maximum in the MovingPeaksLandscape', default=30)
-    argparser.add_argument('--n_peaks', type=int, help='Number of Peaks (optimas) used in Rugged benchmarks like MPL', default=5) 
-
-    # Novelty Archive hyperparameters
-    argparser.add_argument('--archive_nn', type=int, help='Number of nearest neighbors to consider for novelty calculation', default=20)
-    argparser.add_argument('--archive_threshold', type=int, help='Minimum distance threshold for considering behaviors as novel', default=0.1) 
-    argparser.add_argument('--fit_weight', type=float, help='Weight given to pure-fitness calculating individual total fitness', default=1.0)
-    argparser.add_argument('--novelty_weight', type=float, help='Weight given to novelty calculating individual total fitness', default=1.0)
     
     # Genetic Programming variables
     argparser.add_argument('--max_depth', type=int, help='GP Tree maximum depth', default=15)
     argparser.add_argument('--initial_depth', type=int, help='GP Tree maximum depth', default=6)
-    
-    # Rugged arguments
-    argparser.add_argument('--generations', type=int, help='Nº of generations to run the GA. (Used interchangeably with args.dimensions in this case)', default=100)
-    argparser.add_argument('--N_NKlandscape', type=int, help='Genome Length (N) value', default=100)
-    argparser.add_argument('--K_NKlandscape', type=int, help='Nº of interactions per loci (K) value', default=14)
-    argparser.add_argument('--max_kinship', type=float, help='Maximal Inbreeding prevention ratio', default=0.5)
     
     # Parse all arguments
     args = argparser.parse_args()
@@ -67,28 +48,7 @@ def set_args():
     
     return args
 
-# ---------------- NK Landscape visualization helper functions --------------------- #
-
-def build_lineage_graph(lineage_data):
-    G = nx.DiGraph()
-    for data in lineage_data:
-        ind_id = data['id']
-        generation = data['generation']
-        ancestors = data['ancestors']
-        # Add node for the individual
-        G.add_node(ind_id, generation=generation)
-        # Add edges from ancestors to the individual
-        for ancestor_id in ancestors:
-            G.add_edge(ancestor_id, ind_id)
-    return G
-
-def get_lineage_frequency(lineage_data):
-    df = pd.DataFrame(lineage_data)
-    # Explode the ancestors set into individual ancestor IDs
-    df = df.explode('ancestors')
-    # Group by generation and ancestor ID
-    frequency = df.groupby(['generation', 'ancestors']).size().reset_index(name='count')
-    return frequency
+# ---------------- Write/Read helper functions --------------------- #
 
 def save_accuracy(array, file_path_temp):
     file_path = f"{os.getcwd()}/saved_data/" + file_path_temp
@@ -97,71 +57,16 @@ def save_accuracy(array, file_path_temp):
         np.save(f, array)
     print(f"\nAccuracy data saved to {file_path}")
 
-
 # ---------------- Genetic Programming helper functions --------------------- #
 
 def get_function_bounds(benchmark):
     
-    if benchmark == 'ackley':
-        bounds = (-32.768, 32.768)
-    elif benchmark == 'rastrigin' or benchmark == 'sphere':
-        bounds = (-5.12, 5.12)
-    elif benchmark == 'rosenbrock':
-        bounds = (-2.048, 2.048)
-    elif benchmark == 'schwefel':
-        bounds = (-500, 500)
-    elif benchmark == 'griewank':
-        bounds = (-600, 600)   
-    elif benchmark == 'nguyen1' or benchmark == 'nguyen2' or benchmark == 'nguyen3' or benchmark == 'nguyen4' or benchmark == 'nguyen5' or benchmark == 'nguyen6':
+    if benchmark == 'nguyen1' or benchmark == 'nguyen2' or benchmark == 'nguyen3' or benchmark == 'nguyen4' or benchmark == 'nguyen5' or benchmark == 'nguyen6':
         bounds = (-4.0, 4.0)
     elif benchmark == 'nguyen7' or benchmark == 'nguyen8':
         bounds = (0.0, 8.0)
     
     return bounds
-
-def select_benchmark(args):
-    """
-        Definition
-        -----------
-            Select the benchmark function of Optimization class.
-    """
-    benchmarks = {"ackley": bf.ackley_function, "rosenbrock":bf.rosenbrock_function,
-                  "rastrigin": bf.rastrigin_function, "schwefel": bf.schwefel_function,
-                  "griewank" :bf.griewank_function, "sphere": bf.sphere_function}
-    
-    # Get function
-    landscape_fn = benchmarks.get(args.benchmark)
-    
-    # Set the specific bounds
-    args.bounds = get_function_bounds(args.benchmark)
-    
-    return landscape_fn
-
-def get_function_arity(function):
-    arity_dict = {
-        '+': 2,
-        '-': 2,
-        '*': 2,
-        '/': 2,
-        'sin': 1,
-        'cos': 1,
-        'log': 1
-    }
-    return arity_dict.get(function, 0)
-
-def set_config_parameters(benchmark):
-    
-    # Experiment Parameters
-    pop_sizes = [25, 50, 100, 200]
-    dimensions = 10
-    bounds = get_function_bounds(benchmark)
-    generations = 200
-    mutation_rate = 0.2
-    allowed_distance = 1.0
-    
-    return pop_sizes, dimensions, bounds, generations, mutation_rate, allowed_distance
-
-# --------------------------- Genetic Programming visualization ------------------------------- #
 
 def select_gp_benchmark(args):
     """
@@ -181,6 +86,8 @@ def select_gp_benchmark(args):
     args.bounds = get_function_bounds(args.benchmark)
     
     return gp_bench_fn
+
+# --------------------------- Genetic Programming visualization ------------------------------- #
 
 class Node:
     def __init__(self, value, children=None):
@@ -464,37 +371,3 @@ def pad_diversity_lists(results_dict, target_length):
         results_dict[run]['diversity'] = padded_diversity
         print(f"Run {run}: Padded Diversity Lengths = {[len(s) for s in results_dict[run]['diversity']]}")
     return results_dict
-    
-# ---------------------- Rugged Landscape Functions ------------------------- #
-
-def extract_behavior(genome, landscape):
-    
-    # Get all distances
-    distances = [np.sum(genome != peak.position) for peak in landscape.peaks]
-    
-    # Get the minimum distance to any peak for getting closest peak
-    min_distance = min(distances)
-    closest_peak_index = distances.index(min_distance)
-    
-    # Normalize over the genome length
-    normalized_distance = min_distance / landscape.n  
-    behavior = (closest_peak_index, normalized_distance)
-
-    return behavior
-
-def behavior_distance(b1, b2):
-    """
-        Definition
-        -----------
-            Get the behavioral distance between 2 different behaviors of individuals. Maximum is 2.
-            
-        Parameters
-        -----------
-            - b1 and b2 (tuples): (closest_peak_index, normalized_distance)
-    """
-    
-    peak_index_distance = 0 if b1[0] == b2[0] else 1
-    distance_difference = abs(b1[1] - b2[1])
-    
-    # You can weight the components differently if needed
-    return peak_index_distance + distance_difference
